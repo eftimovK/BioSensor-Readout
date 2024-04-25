b7537f5ad89a65e643c9732ace7f09e0576e5861
Kostadin <eftimovk99@gmail.com>
2024-04-25 11:30:09 +0200

    Script for calibration values for impedance measurement

---
+++ RC_impedance_optimization.m
@@ -0,0 +1,54 @@
+% Impedance measurement optimization
+% by setting the p2p excitation 
+% output voltage, the RTIA/CTIA combination, 
+% and the calibration resistor.
+%
+% In accordance with the equations from the manual
+% "Optimizing the ADuCM350 for Impedance Conversion"
+
+
+f = 1e3; % [Hz]
+
+omega = 2*pi*f;
+
+Rs = 1e3;
+Rp = 10*1e3;
+Cp = 220 * 1e-12;
+
+
+Z_Cp = -1i * 1 / (omega * Cp);
+
+Z_tot = Rs + 1/(1/Rp + 1/Z_Cp);
+% Z_tot = Rs*1;
+
+Z_tot_min = abs(Z_tot);
+R_cal = Z_tot_min           % [A]
+
+angle(Z_tot) * 180 / pi;
+
+
+% Maximum voltage swing is 600 mV peak
+% set it below or equal to this value:
+V_swing_max = 10 / 1e3       % [V]
+
+% Highest signal current into TIA
+I_max_tia = V_swing_max / R_cal; % [A]
+
+
+% Peak voltage at output of TIA 
+% (maximum allowed by the ADuCM350) = 750 mV peak.
+% 
+% constant, i.e. cannot be modified...?
+V_peak_tia = 750 / 1000;  % [V]
+
+
+% Transimpedance resistor
+R_tia = V_peak_tia / I_max_tia
+
+% anti-aliasing capacitor
+f_bw = 80*1e3; % max. system bandwidth [Hz]
+
+C_tia = 1 / (2*pi*f_bw * R_tia)
+
+
+
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Project Information
 
 ## Brief Description
+As part of the [SensUs competition](https://www.sensus.org/), the goal is to develop a readout system for the biosensor of team AixSense, RWTH Aachen. 
+
+This repository contains therefore all necessary resources and code required for the readout system, using the [ADuCM350 device](https://www.analog.com/en/products/aducm350.html). 
 
 ## Important Links
+
+- Comprehensive forum for ADuCM350: https://ez.analog.com/analog-microcontrollers/precision-microcontrollers/w/documents/2432/aducm350-faq
